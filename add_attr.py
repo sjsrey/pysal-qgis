03e3bcd1ee246dfef9c4558ca955cd33fad54d33
kkane5 <kkane5@asu.edu>
2012-11-29 17:16:03 -0700

    Code writes a numpy array to a qgis shapefile

--- add_attr.py
+++ add_attr.py
@@ -7,11 +7,22 @@ provider = layer.dataProvider()
 feat = QgsFeature()
 caps = layer.dataProvider().capabilities()
 if caps & QgsVectorDataProvider.AddAttributes:
-	res = layer.dataProvider().addAttributes([QgsField("myint",QVariant.Int)])
-# Changing Attribute Values
-if caps & QgsVectorDataProvider.ChangeAttributeValues:
-  attrs = { 0 : QVariant("hello"), 1 : QVariant(123) }
-  layer.dataProvider().changeAttributeValues({ fid : attrs })
+ res = layer.dataProvider().addAttributes([QgsField("LISA",QVariant.Int)])
 
-# try a QVariant of float ... for I's and p-values
-# figure out update
+# This will come from pysal - placeholder for now
+cluster = range(layer.featureCount())
+for i in cluster:
+ cluster[i] = cluster[i]*1.5
+
+# This is the part that does all the work
+a = range(layer.featureCount())
+d = dict(zip(a,cluster)
+n = len(allAtt) - 1
+allAtt = provider.attributeIndexes()
+for i in a:
+ fid = i
+ if caps & QgsVectorDataProvider.ChangeAttributeValues:
+  attrs = { n : QVariant(d[i])}
+  layer.dataProvider().changeAttributeValues({ fid : attrs })
+layer.commitChanges()
+
